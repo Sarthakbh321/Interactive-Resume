7c16db2338800ee6d6b4a572b3ae8a16987a16fe
Sarthak <sarthakbh321@gmail.com>
2020-08-29 23:31:11 +0530

    local storage for interactivity

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Title from "./components/Title";
 import Navbar from "./components/Navbar/Navbar";
@@ -6,8 +6,24 @@ import Navbar from "./components/Navbar/Navbar";
 function App() {
 	const [interactive, setInteractive] = useState(true);
 
+	useEffect(() => {
+		let inter = localStorage.getItem("interactive");
+
+		if (inter === null) {
+			setInteractive(true);
+		} else {
+			if (inter === "true") {
+				setInteractive(true);
+			} else {
+				setInteractive(false);
+			}
+		}
+	}, []);
+
 	const handleSwitch = (event) => {
 		setInteractive(event.target.checked);
+
+		localStorage.setItem("interactive", event.target.checked);
 	};
 
 	return (
