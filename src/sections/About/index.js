32fe1cfb09ac16ddf9dea14a71ca356f20fb96d2
Sarthak <sarthakbh321@gmail.com>
2020-08-31 00:45:23 +0530

    completed about

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -7,7 +7,23 @@ function About() {
 		<Container className="about-section">
 			<h1 className="about-head">About Me</h1>
 			<div className="about-content">
-				<Grid container sm={12} md={6}></Grid>
+				<Grid container className="about-grid">
+					<Grid item sm={12} md={6} className="about-text-div">
+						Hello! I am Sarthak Bharadwaj, currently a 2nd year
+						student in Vellore Institute of Technology, Vellore.
+						From very early on in my life, I have been interested in
+						technology and computers. Hence, I am currently pursuing
+						this interest as a Computer Science student. <br />{" "}
+						<br />
+						My technical interests know no bounds! I am good at
+						Frontend Development, Competititve Programming, also
+						know something about App Development and Backend
+						Development.
+					</Grid>
+					<Grid item xs={12} sm={6} className="profile-img-div">
+						<img src="profile.jpeg" height={350} alt="my face" />
+					</Grid>
+				</Grid>
 			</div>
 		</Container>
 	);
