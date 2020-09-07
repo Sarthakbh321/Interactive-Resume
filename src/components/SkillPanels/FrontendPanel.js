e3a38e85696d5b9f578491cb28ba8568d21e11b5
Sarthak <sarthakbh321@gmail.com>
2020-09-07 21:22:17 +0530

    started on frontend panel

--- src/components/SkillPanels/FrontendPanel.js
+++ src/components/SkillPanels/FrontendPanel.js
@@ -1,9 +1,33 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import "./Panel.css";
+import { Grid } from "@material-ui/core";
 
 function FrontendPanel() {
-	return <div>Frontend skills</div>;
+	return (
+		<div style={{ flexGrow: 1 }}>
+			<Grid container spacing={3}>
+				<Grid item xs={6} sm={3}>
+					Hello
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					Hello
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					Hello
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					Hello
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					Hello
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					Hello
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default FrontendPanel;
