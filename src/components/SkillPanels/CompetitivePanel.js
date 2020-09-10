577889a5868ab7b85ac909aa7423eaa93beb9587
Sarthak <sarthakbh321@gmail.com>
2020-09-10 22:14:48 +0530

    done competitive panel

--- src/components/SkillPanels/CompetitivePanel.js
+++ src/components/SkillPanels/CompetitivePanel.js
@@ -1,8 +1,25 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { Grid } from "@material-ui/core";
+import SkillImage from "./SkillImage";
+import "./Panel.css";
 
 function CompetitivePanel() {
-	return <div>Competitive Panel</div>;
+	return (
+		<div style={{ flexGrow: 1 }}>
+			<Grid container spacing={2}>
+				<Grid item xs={6} sm={4} className="skill-item">
+					<SkillImage src="python" name="Python" />
+				</Grid>
+				<Grid item xs={6} sm={4} className="skill-item">
+					<SkillImage src="cpp" name="C++" />
+				</Grid>
+				<Grid item xs={6} sm={4} className="skill-item">
+					<SkillImage src="java" name="Java" />
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default CompetitivePanel;
