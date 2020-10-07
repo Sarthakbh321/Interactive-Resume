a923d8e6ebb122fb9a8d6f13d3623f7bf7c613ae
Sarthak <sarthakbh321@gmail.com>
2020-10-07 23:21:01 +0530

    contact finished

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Contact.css";
+import { Grid } from "@material-ui/core";
 
 function Contact() {
 	return (
@@ -10,50 +11,90 @@ function Contact() {
 					👋
 				</span>
 			</h1>
-			<div className="contact-section">
-				<p className="img-text">
-					<a
-						href="mailto:sarthakbh321@gmail.com"
-						className="blue-color"
-					>
-						sarthakbh321@gmail.com
-					</a>{" "}
-					<img
-						src="./icons/gmail.svg"
-						width={40}
-						height={40}
-						alt="gmail icon"
-					/>
-				</p>
-				<p className="img-text">
-					<a
-						href="https://www.linkedin.com/in/sarthak-bharadwaj-8552b5110/"
-						className="blue-color"
-					>
-						Sarthak Bharadwaj
-					</a>{" "}
-					<img
-						src="./icons/linkedin.svg"
-						width={40}
-						height={40}
-						alt="linkedin icon"
-					/>
-				</p>
-				<p className="img-text">
-					<a
-						href="https://www.instagram.com/sarthak_bharadwaj_/"
-						className="blue-color"
-					>
-						@sarthak_bharadwaj_
-					</a>{" "}
-					<img
-						src="./icons/instagram.svg"
-						width={40}
-						height={40}
-						alt="instagram icon"
-					/>
-				</p>
-			</div>
+			<Grid container>
+				<Grid item xs={12} sm={6} className="contact-desc">
+					<p className="contact-text">
+						Thanks for visiting my portfolio! If you liked it, you
+						can star it on Github if you want. The repo can be found{" "}
+						<a
+							style={{ textDecoration: "none" }}
+							className="blue-color"
+							href="https://github.com/Sarthakbh321/Interactive-Resume"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							here
+						</a>
+						.
+					</p>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<div className="contact-section">
+						<p className="img-text">
+							<a
+								href="https://github.com/Sarthakbh321"
+								className="blue-color"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								@Sarthakbh321
+							</a>
+							<img
+								src="./icons/github.svg"
+								width={40}
+								height={40}
+								alt="github icon"
+							/>
+						</p>
+						<p className="img-text">
+							<a
+								href="mailto:sarthakbh321@gmail.com"
+								className="blue-color"
+							>
+								sarthakbh321@gmail.com
+							</a>{" "}
+							<img
+								src="./icons/gmail.svg"
+								width={40}
+								height={40}
+								alt="gmail icon"
+							/>
+						</p>
+						<p className="img-text">
+							<a
+								href="https://www.linkedin.com/in/sarthak-bharadwaj-8552b5110/"
+								className="blue-color"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Sarthak Bharadwaj
+							</a>{" "}
+							<img
+								src="./icons/linkedin.svg"
+								width={40}
+								height={40}
+								alt="linkedin icon"
+							/>
+						</p>
+						<p className="img-text">
+							<a
+								href="https://www.instagram.com/sarthak_bharadwaj_/"
+								className="blue-color"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								@sarthak_bharadwaj_
+							</a>{" "}
+							<img
+								src="./icons/instagram.svg"
+								width={40}
+								height={40}
+								alt="instagram icon"
+							/>
+						</p>
+					</div>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
